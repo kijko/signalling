e60a04564cd9be630219b6e339d895252c3cec83
mkijko <kijkomaciej@gmail.com>
2020-12-20 10:55:46 +0100

    change main class for gradle

--- src/main/java/pl/kijko/Signalling.java
+++ src/main/java/pl/kijko/Signalling.java
@@ -4,9 +4,11 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import pl.kijko.sectormanager.Main;
 import pl.kijko.sectors.Sectors;
 
+import java.io.IOException;
+
 public class Signalling {
 
-    public static void main(String[] args) throws MqttException {
+    public static void main(String[] args) throws MqttException, IOException {
         Main.main(new String[]{});
         Sectors.main(new String[]{});
     }
